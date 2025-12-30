ce99d08a047823f95c0174e4d3c3f90a2cdf95c4
Johnatas michaell <johnatasmichael067@gmail.com>
2025-12-30 09:17:57 -0300

    initial_project

---
+++ script.js
@@ -0,0 +1,9 @@
+// Seleciona os elementos do formulário 
+
+const amount = document.getElementById('amount')
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, "")
+  
+  amount.value = value
+}
